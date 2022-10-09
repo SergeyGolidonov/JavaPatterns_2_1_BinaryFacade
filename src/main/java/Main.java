6796abbcba779d196bbc6f4f5c2c78277425911a
Sergey Golidonov <3apa3a86@inbox.ru>
2022-10-09 10:33:00 +0300

    Task 'BinaryFacade' is done

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        // Демонстрация сложения
+        System.out.println(bins.sum("1001", "1101"));
+        // Демонстрация умножения
+        System.out.println(bins.mult("1001", "1101"));
+    }
+}
